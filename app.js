8e3083e894d0a2026b7b900a8af5995d1d453164
HoanghoDev <110652388+HoanghoDev@users.noreply.github.com>
2023-12-16 17:34:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,69 +1,68 @@
 let list = document.querySelectorAll('.carousel .list .item');
 let carousel = document.querySelector('.carousel');
-let dots = document.querySelectorAll('.carousel .dots li');
+let dots = document.querySelectorAll('.dots li');
 let nextBtn = document.getElementById('next');
 let prevBtn = document.getElementById('prev');
 
-let countItem = list.length;
+let lastPosition = list.length - 1;
 let active = 0;
 let zIndex = 2;
 
-
 nextBtn.onclick = () => {
-    let newActive = active + 1 >= countItem ? 0 : active + 1;
-    setItemActive(newActive, showSlider);
+    let newValue = active + 1 > lastPosition ? 0 : active + 1;
+    setItemActive(newValue, showSlider);
 }
 prevBtn.onclick = () => {
-    let newActive = active - 1 < 0 ? countItem - 1 : active - 1;
-    setItemActive(newActive, showSlider);
+    let newValue = active - 1 < 0 ? lastPosition : active - 1;
+    setItemActive(newValue, showSlider);
 }
-const setItemActive = (newActive, callbackFunction) => {
-    if(newActive === active) return;
-    let buttonClick = 'next';
-    if(newActive < active) buttonClick = 'prev';
-    active = newActive;
-    callbackFunction(buttonClick);
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        setItemActive(index, showSlider);
+    })
+})
+
+const setItemActive = (newValue, callbackFunction) => {
+    if(newValue === active) return;
+    let type = newValue > active ? 'next' : 'prev';
+    active = newValue;
+    callbackFunction(type);
 }
 let removeEffect;
-let unAcceptClick;
 let autoRun = setTimeout(() => {
     nextBtn.click();
 }, 5000);
 const showSlider = (type) => {
     carousel.style.pointerEvents = 'none';
-    let activeOld = document.querySelector('.carousel .item.active');
-    if(activeOld) activeOld.classList.remove('active');
-    list[active].classList.add('active');
+    // find Item Active Old
+    let itemActiveOld = document.querySelector('.carousel .list .item.active');
+    if(itemActiveOld) itemActiveOld.classList.remove('active');
     zIndex++;
     list[active].style.zIndex = zIndex;
+    list[active].classList.add('active');
+
     if(type === 'next'){
         carousel.style.setProperty('--transform', '300px');
     }else{
         carousel.style.setProperty('--transform', '-300px');
     }
     carousel.classList.add('effect');
-    // li
-    let dotActiveOld = document.querySelector('.carousel .dots li.active');
-    dotActiveOld.classList.remove('active');
+
+    // dots
+    // find dot active old
+    let dotActiveOld = document.querySelector('.dots li.active');
+    if(dotActiveOld) dotActiveOld.classList.remove('active');
     dots[active].classList.add('active');
 
     clearTimeout(removeEffect);
-    removeEffect = setTimeout(()=>{
+    removeEffect = setTimeout(() => {
         carousel.classList.remove('effect');
-    }, 1500)
-    clearTimeout(unAcceptClick);
-    unAcceptClick = setTimeout(() => {
         carousel.style.pointerEvents = 'auto';
-    },2000)
+    }, 1500);
+
     clearTimeout(autoRun);
     autoRun = setTimeout(() => {
         nextBtn.click();
     }, 5000);
 }
-
-dots.forEach((li, index) => {
-    li.addEventListener('click', () => {
-        let newActive = index;
-        setItemActive(newActive, showSlider);
-    })
-})
